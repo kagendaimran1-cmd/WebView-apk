a386e5aba4d48d307dbfc0caf0288c598b1e1647
kagendaimran1-cmd <your-email@example.com>
2026-04-27 17:53:48 +0300

    Added Android project files

---
+++ app/src/main/java/com/example/webviewapk/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.webviewapk;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://google.com");
+
+        setContentView(webView);
+    }
+}
